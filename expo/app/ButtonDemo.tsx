4dc651b30110ac41faaead57692260c67d5ddd06
satish-solera <satishsolera54@gmail.com>
2026-01-09 09:02:12 +0530

    add: button demo

---
+++ expo/app/ButtonDemo.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import { Button } from "./components";
+
+
+export default function ButtonDemo() {
+    return (
+        <View>
+            <Button>
+                Sol Ui
+            </Button>
+        </View>
+    )
+}

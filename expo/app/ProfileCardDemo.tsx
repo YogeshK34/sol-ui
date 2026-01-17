7f1f7c11391fa3b816f0fb0f6b5d436df066f08e
satish-solera <satishsolera54@gmail.com>
2026-01-17 22:02:40 +0530

    fix: profile card demo

--- expo/app/ProfileCardDemo.tsx
+++ expo/app/ProfileCardDemo.tsx
@@ -7,7 +7,7 @@ import ProfileCard from "./components/ui/profile-card";
 export default function ProfileCardDemo() {
     return (
         <View className='flex items-center justify-center min-h-screen'>
-           <ProfileCard src="https://www.pexels.com/photo/woman-wearing-black-spaghetti-strap-top-415829/" subTitle='@sol-auth' title='Satish' />
+           <ProfileCard src="https://www.pexels.com/photo/woman-wearing-black-spaghetti-strap-top-415829/" subTitle='@sol-auth' title='sonali' />
         </View>
     )
 }

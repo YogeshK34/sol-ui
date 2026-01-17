f432f1d111a71ede556f2950cd6b6c94ba215f23
Satish Solera <satishsolera54@gmail.com>
2026-01-17 21:13:40 +0530

    Update DiscreteTabsDemo.tsx

--- expo/app/DiscreteTabsDemo.tsx
+++ expo/app/DiscreteTabsDemo.tsx
@@ -3,10 +3,11 @@ import { View } from "react-native";
 import DiscreteTabs from "./components/ui/discrete-Tabs";
 
 
-export default function FilterDemo() {
+export default function DiscreteTabsDemo() {
     return (
         <View className='flex items-center justify-center min-h-screen'>
          <DiscreteTabs />
         </View>
     )
+
 }

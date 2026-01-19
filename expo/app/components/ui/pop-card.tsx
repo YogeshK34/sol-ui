8c926fa60182516ff43e24a8e65c8eb854a7519e
satish-solera <satishsolera54@gmail.com>
2026-01-19 14:08:05 +0530

    fix: pop-card ui

--- expo/app/components/ui/pop-card.tsx
+++ expo/app/components/ui/pop-card.tsx
@@ -7,13 +7,11 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import Feather from 'react-native-vector-icons/Feather'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-
-
 export default () => {
     const [pop, setPop] = useState(false)
     return (
         <View className="m-10">
-            <Pressable onPress={() => setPop((prev) => !prev)}>
+            <Pressable className="flex flex-row border border-black/10 px-3 py-1 items-center gap-2 rounded-md" onPress={() => setPop((prev) => !prev)}>
                 <Text className="text-4xl">
                     : 
                 </Text>
@@ -23,16 +21,12 @@ export default () => {
             </Pressable>
             {
                 pop && (
-                    <View className=" w-52 rounded-md  bg-violet-400  border border-neutral-300 shadow-md absolute  top-10 overflow-y-scroll ">
-
+                    <View className=" w-60 rounded-md  bg-violet-400  border border-neutral-300 shadow-md absolute  top-10 overflow-y-scroll ">
                         <View className="flex flex-row bg-neutral-100">
-                          
                             <TopPart icon={<Feather name="star" size={20}/>} />
-                            
                         </View>
 
                         <View className="">
-                      
                             <LabelButton title="History" icon={<MaterialCommunityIcons name="history" size={18} />} />
                             <View className="border-b"> </View>
                             <LabelButton title="Downloads" icon={<AntDesign name="download" size={18} />} />
@@ -58,7 +52,7 @@ export default () => {
 
 export const LabelButton = ({ title, icon }: { title: string, icon?: React.ReactNode }) => {
     return (
-        <Pressable className="p-2 active:bg-violet-200/20  ">
+        <Pressable className="w-60 p-2 py-3 border border-black/5 active:bg-violet-200/20  ">
             <View className="flex flex-row items-center gap-3 text-md font-normal  " >
                 <Text className="w-6">{icon}</Text>
                 <Text className="text-white font-medium"> {title}</Text>

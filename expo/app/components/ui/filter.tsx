3b9d09aa8e3e51541f39fe68cb3b113822780e3e
satish-solera <satishsolera54@gmail.com>
2026-01-19 14:08:33 +0530

    fix: filter ui layout

--- expo/app/components/ui/filter.tsx
+++ expo/app/components/ui/filter.tsx
@@ -27,13 +27,13 @@ export function Filter({ filterButtons, addFilters }: FilterProps) {
 
     return (
         <View className="h-40 w-80 shadow-md rounded-xl m-10 border ">
-            < Text className="p-4 font-bold flex items-center">
-               Apply filter
-                <AntDesign name="filter" size={15}/>
+            < Text className="p-4 font-bold flex flex-row items-center  gap-4">
+                <Text > Apply filter</Text>
+                <AntDesign name="filter" size={15} />
             </Text>
 
 
-            <View className="flex flex-row gap-2 m-2">
+            <View className="flex flex-row gap-2 m-2 items-center">
                 {
                     filterButtons.map((el, id) => (
                         <FilterButton key={id} label={el.label} icon={el.icon} />
@@ -41,34 +41,27 @@ export function Filter({ filterButtons, addFilters }: FilterProps) {
                 }
 
                 <View className="">
-                    <Pressable onPress={() => setFilter((prev) => !prev)} className={`bg-red-300 size-8 flex items-center justify-center rounded-md '
-                        ${filter ? "bg-red-300" : "bg-green-300"}
+                    <Pressable onPress={() => setFilter((prev) => !prev)} className={`bg-black text-white size-8 flex items-center justify-center rounded-md '
+                        ${filter ? "bg-black/80" : "bg-black "}
                     `}
                     >
-                        <Text className="m-auto">
+                        <Text className="m-auto text-white">
                             {
                                 filter ? 'X' : '+'
                             }
                         </Text>
                     </Pressable>
 
-                    {addFilters.length > 0 && filter && <View className="shadow-md bg-white min-w-28 absolute left-10 top-2 rounded-md p-1">
+                    {addFilters.length > 0 && filter && <View className="w-20 shadow-md bg-white min-w-28 absolute left-10 top-2 rounded-md p-1 gap-2">
                         {
                             addFilters.map((el, id) => (
                                 <AddFilter key={id} label={el.label} />
                             ))
                         }
 
-
                     </View>}
                 </View>
             </View>
-
-
-
-
-
-
         </View>
     )
 }
@@ -77,7 +70,7 @@ export function Filter({ filterButtons, addFilters }: FilterProps) {
 export const FilterButton = ({ label, icon }: FilterButtons) => {
 
     return (
-        <Pressable className="bg-violet-300/40  py-2 px-3 rounded-md h-fit ">
+        <Pressable className="border border-black/10  py-2 px-6 rounded-md h-fit ">
             <Text className="flex items-center gap-1 mx-auto"
             ><Text>
                     {icon}
@@ -88,7 +81,7 @@ export const FilterButton = ({ label, icon }: FilterButtons) => {
 
 export const AddFilter = ({ label, icon }: AddFilters) => {
     return (
-        <View className="flex flex-row items-center gap-1 justify-between active:bg-violet-300/30 p-2 rounded-md">
+        <View className="w-30 flex flex-row items-center gap-1 justify-between  p-2 rounded-md border border-black/10 active:bg-black/5 ">
             <Text>
                 {
                     icon

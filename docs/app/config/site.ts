72ac3c73d0637e9df5afc8807128e86d1b119d52
satish-solera <satishsolera54@gmail.com>
2026-01-27 20:11:47 +0530

    fix: site config

--- docs/app/config/site.ts
+++ docs/app/config/site.ts
@@ -3,7 +3,7 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "solUI - Beautiful, fast and modern React Native UI Library",
   description: "Make Your Android Screens Better.",
-  ogImage: "/solui.png",
+  ogImage: "https://solui-cyan.vercel.app/solui.png",
   author: "Satish Solera",
   siteUrl: "https://solui-cyan.vercel.app",
   creator: "@solui",
@@ -26,7 +26,7 @@ export const siteConfig = {
     card: "summary_large_image",
     title: "solUI - Beautiful, fast and modern React Native UI Library",
     description: "Make Your Android Screens Better.",
-    image: "/solui.png",
+    image: "https://solui-cyan.vercel.app/solui.png",
     creator: "@solui",
   },
   links: {

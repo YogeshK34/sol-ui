7bdb6bb711bfe334a87c19b67cacf67dd25a494d
satish-solera <satishsolera54@gmail.com>
2026-01-30 19:32:30 +0530

    add: block preview for images

---
+++ docs/src/components/ui/block-preview.tsx
@@ -0,0 +1,11 @@
+
+
+export const blockPreview = (src?: string) => {
+   
+    return (
+        <div className="grid grid-cols-2 gap-2">
+            <img src={src} width={200} height={200} />
+        </div>
+    )
+}
+// /ui-demo-preview/item-cart-preview-2.jpg
